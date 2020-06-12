978a084b712d78263b897a7b422216854fb182fc
Harald <jose_mejias92@hotmail.com>
2020-06-12 14:26:39 +0200

    validacion correo, email, nombre

--- my-agenda-app/src/app/app.component.ts
+++ my-agenda-app/src/app/app.component.ts
@@ -9,7 +9,14 @@ export class AppComponent {
   contactContainer = [];
   nombre : string;
   email : string;
-  telefono : number;
+  telefono : string;
+  error: boolean = false;
+  re =new RegExp("/^([0-9])*$/");
+  validacionNombre: string = "";
+  validacionEmail: string = "";
+  validacionTelefono: string = "";
+  result: any;
+  
 
   addContact(){
     this.contactContainer.push( {
@@ -24,6 +31,30 @@ export class AppComponent {
 
 
   }
+
+  onBlur(){
+    if (this.nombre.length<6) {
+      this.error = true;
+      this.validacionNombre = "Nombre proporcionado muy corto";
+    }
+    if (!this.email.includes("@")) {
+      this.error =  true;
+      this.validacionEmail = "El email es incorrecto";
+    }
+    if (!this.telefono.match(this.re) && this.telefono.length >= 9 ){
+      this.error = true;
+      this.validacionTelefono = "El telefono es incorrecto";
+    }
+    if (this.contactContainer.length> 0){ 
+      
+      var result = this.contactContainer.filter(contact => contact.email === this.email)
+
+      
+      console.log(result)
+    }
+  }
+
+
 
 
   deteleContacs(){
